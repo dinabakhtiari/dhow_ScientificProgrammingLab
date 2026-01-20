0c19cf917b900598f2657a65c7fb1b10d3477a74
dinabakhtiari <142736449+dinabakhtiari@users.noreply.github.com>
2026-01-20 16:08:43 +0000

    working with arrays 2

--- array_creation.py
+++ array_creation.py
@@ -1,6 +1,17 @@
 import numpy as np
-#my_array = np.array([10, 9, 8])
+my_array = np.array([10, 9, 8])
 #print(my_array)
 
-my_2d_array = np.array([[1,3,5],[2,4,6]])
+my_2d_array = np.array([[1, 3, 5, 7],[2, 4, 6, 8]], dtype=np.float32)
 print(my_2d_array[1, 1])
+print("-------------")
+print(my_2d_array[1])
+print("-------------")
+print(my_2d_array[:, 2])
+print("-------------")
+print(my_array.shape)
+print(my_2d_array.shape)
+print("-------------")
+print(my_2d_array.dtype)
+print("-------------")
+
